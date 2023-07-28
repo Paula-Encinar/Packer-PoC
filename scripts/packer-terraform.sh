7b13b051935ab0905ca5049ec70b63b45e9f97b8
Paula-Encinar <pencinarsanz@gmail.com>
2023-07-28 09:46:06 +0200

    sed command error

--- scripts/packer-terraform.sh
+++ scripts/packer-terraform.sh
@@ -47,7 +47,7 @@ case "$1" in
       echo ""
 
       echo "Change AMI variable to Deploy"
-      sed -i '/packer_ami_id/s/.*/packer_ami_id="'$AMI_ID'"/' $terradeploypath/terraform.tfvars
+      sed '/packer_ami_id/s/.*/packer_ami_id="'$AMI_ID'"/' $terradeploypath/terraform.tfvars
       echo ""
       
       echo "terraform init => packer deploy infrastructure"
